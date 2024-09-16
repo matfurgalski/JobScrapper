143ceb990673f37b16151554973abd80277779a5
Mateusz Furgalski <mateusz.furgalskii@gmail.com>
2024-09-16 23:30:04 +0200

    change axios ip

--- job-scrapper-frontend/src/axios-client.js
+++ job-scrapper-frontend/src/axios-client.js
@@ -1,7 +1,14 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: `http://host.docker.internal:8080/api/v1/`
+  baseURL: `http://192.168.1.10:8080:8080/api/v1/`,
+   headers: {
+      'Content-Type': 'application/json',
+      // Add any additional headers needed
+      'Accept-Encoding': 'gzip, deflate',
+      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/png,image/svg+xml,*/*;q=0.8',
+      'Connection': 'keep-alive',
+    }
 })
 
 
